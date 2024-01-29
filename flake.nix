bf9926e18835e0f8754886659abc874e6d91fd7a
Linerre <leon.errelin@outlook.com>
2024-01-29 22:46:32 +1100

    Init: setup dev env

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Race Protocol on Sui blockchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    sui-overlay = {
+      url = "github:Linerre/sui-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, sui-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ sui-overlay.overlays.${system}.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+        with pkgs;
+        {
+          devShell = mkShell {
+            buildInputs = [
+              sui-devnet # or sui-testnet, sui-mainnet
+              just
+            ];
+          };
+        }
+    );
+}
